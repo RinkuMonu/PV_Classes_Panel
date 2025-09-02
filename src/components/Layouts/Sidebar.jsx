2cf5c47950b06e124bbccf10364fcdafc4464446
rahul7425 <rahuljsrasr1112@gmail.com>
2025-09-02 17:21:25 +0530

    rr

--- src/components/Layouts/Sidebar.jsx
+++ src/components/Layouts/Sidebar.jsx
@@ -215,7 +215,7 @@ const Sidebar = ({ isCollapsed }) => {
     { name: "FAQ", Icon: Calendar, path: "/faq" },
     { name: "Current Affairs", Icon: Calendar, path: "/affairs" },
     { name: "Users", Icon: Users, path: "/users" },
-    { name: "Doubts", Icon: HelpCircle, path: "/doubts" },
+    { name: "DoubtsTable", Icon: HelpCircle, path: "/doubt" },
     { name: "Notification", Icon: Bell, path: "/notification" },
     { name: "Orders", Icon: CreditCard, path: "/orders" },
   ];
